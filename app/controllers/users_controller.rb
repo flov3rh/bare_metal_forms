7c0ce6b038cf476adb4e46dc702421daa05c3d69
Juan Pablo <jpablomgil@gmail.com>
2019-12-05 15:16:45 -0600

    adding rhe white listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    #
-    # if @user.save
-    #   redirect_to new_user_path
-    # else
-    #   render :new
-    # end
+    @user = User.new(user_param)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
+private
+
   def user_param
-     params.require(:post).permit(:username,:email,:password)
+    params.require(:user).permit(:username,:email,:password)
   end
 end
