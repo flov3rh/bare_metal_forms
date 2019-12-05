6c9b28e5bb4778d257fd733af878616d7062a8f7
Juan Pablo <jpablomgil@gmail.com>
2019-12-05 14:48:25 -0600

    Creating method user_param con user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    #
+    # if @user.save
+    #   redirect_to new_user_path
+    # else
+    #   render :new
+    # end
+  end
 
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new
-    end
+  def user_param
+     params.require(:post).permit(:username,:email,:password)
   end
 end
