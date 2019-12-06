23efac7a8b44a129fad80f6242c7fc2e92c861cb
Juan Pablo <jpablomgil@gmail.com>
2019-12-06 09:50:55 -0600

    Show errors on wrong input

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :username, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 4 }
 end
